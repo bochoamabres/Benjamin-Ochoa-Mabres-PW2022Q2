4d89f0054672c4c7d4c1dd1bac21d23df60f4637
bochoamabres <117176958+bochoamabres@users.noreply.github.com>
2022-11-20 20:06:16 -0300

    push
    puh¿sh

--- Trabajo-Final/script.js
+++ Trabajo-Final/script.js
@@ -1,1 +1,13 @@
-function add(pro)
+let products = []
+let total = 0
+
+function add(product, price) {
+    console.log(product,price);
+    products.push(product);
+    total = total + price;
+    document.getElementById("checkout").innerHTML = 'Pagar $ {total}';
+}
+
+function pay() {
+    window.alert(products.join(","));
+}
